3767259fd6eea5f7f939fa883992328dfd149101
Saeedpk1 <saeedmci@gmail.com>
2026-09-08 12:14:28 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "pi-timesheets-v2";
+const CACHE_NAME = "pi-timesheets-v3";
 const APP_SHELL = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (event) => {
